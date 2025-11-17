45b226f2a535a7d6da312bbecabbeccad99d367f
Kocsis Gergely <kocsisger@example.com>
2025-11-17 11:05:13 +0100

    Initial commit

---
+++ src/main/java/en/unideb/inf/bootstrap_m10/BootStrapM10Application.java
@@ -0,0 +1,13 @@
+package en.unideb.inf.bootstrap_m10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStrapM10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootStrapM10Application.class, args);
+    }
+
+}
---
+++ src/test/java/en/unideb/inf/bootstrap_m10/BootStrapM10ApplicationTests.java
@@ -0,0 +1,13 @@
+package en.unideb.inf.bootstrap_m10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootStrapM10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
